8f2af0dccfdaed41fb8cf868664770e90bd15b01
EC2 Default User <ec2-user@ip-172-31-39-238.ec2.internal>
2022-05-11 12:56:26 +0000

    第5章完了

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_05_03_125313) do
 
   create_table "lists", force: :cascade do |t|
-    t.string "tite"
+    t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
